19ec8d5c6f2cdcd07b1b9284072a50b877c8fc8e
gitpher <jun_chois@naver.com>
2022-11-07 18:07:29 +0900

    siple login page finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
+// refactor를 통해 적절하게 wrap
+// body : SingleChildScrollView
+// double.infinity
+// Text()와 TextField()
+// padding() & margin(), 그리고 EdgeInsets.all() 또는 .only()
+// Image.network("")
+// 버튼 종류: ElevatedButton, TextButton, IconButton
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,23 +20,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Text(
-              'Hello Flutter',
-              style: TextStyle(
-                fontSize: 28,
-              ),
+        appBar: AppBar(
+          title: Text(
+            'Hello Flutter',
+            style: TextStyle(
+              fontSize: 28,
             ),
-            TextField(
-              decoration: InputDecoration(labelText: '이메일'),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    "https://media.nichegamer.com/wp-content/uploads/2019/06/24135959/razor-ramone-hard-gay-06-24-19-1.jpg",
+                    width: double.infinity,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: '이메일'),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: '비밀번호'),
+                ),
+                Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(top: 16),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('로그인'),
+                    ))
+              ],
             ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: '비밀번호'),
-            )
-          ],
+          ),
         ),
       ),
     );
